90e3e59209362c725ab6a93e57340581de2a8e25
Sabin <spradh@users.noreply.github.com>
2023-12-22 16:07:59 -0600

    Create 3 Build a classification model.py

---
+++ 2-Time-Series-as-Inputs-to-a-Model/3 Build a classification model.py
@@ -0,0 +1,9 @@
+from sklearn.svm import LinearSVC
+
+# Initialize and fit the model
+model = LinearSVC()
+model.fit(X_train, y_train)
+
+# Generate predictions and score them manually
+predictions = model.predict(X_test)
+print(sum(predictions == y_test.squeeze()) / len(y_test))
