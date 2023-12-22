4a53fbfe206b55202e140bb25b3e4a3784a36748
Sabin <spradh@users.noreply.github.com>
2023-12-21 21:38:53 -0600

    Create 03-Fitting a simple model: classification.py

---
+++ 1-Time-Series-and-Machine-Learning-Primer/03-Fitting a simple model: classification.py
@@ -0,0 +1,13 @@
+# Print the first 5 rows for inspection
+print(data.head())
+
+
+from sklearn.svm import LinearSVC
+
+# Construct data for the model
+X = data[['petal length (cm)', 'petal width (cm)']]
+y = data[['target']]
+
+# Fit the model
+model = LinearSVC()
+model.fit(X, y)
