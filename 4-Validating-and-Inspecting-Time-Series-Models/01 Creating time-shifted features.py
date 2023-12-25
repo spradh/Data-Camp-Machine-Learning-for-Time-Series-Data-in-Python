7a4a4d7d33a3aa55344b371c70dd6bd8588def73
Sabin <spradh@users.noreply.github.com>
2023-12-25 10:42:45 -0600

    Create 01 Creating time-shifted features.py

---
+++ 4-Validating-and-Inspecting-Time-Series-Models/01 Creating time-shifted features.py
@@ -0,0 +1,14 @@
+# These are the "time lags"
+shifts = np.arange(1, 11).astype(int)
+
+# Use a dictionary comprehension to create name: value pairs, one pair per shift
+shifted_data = {"lag_{}_day".format(day_shift): prices_perc.shift(day_shift) for day_shift in shifts}
+
+# Convert into a DataFrame for subsequent use
+prices_perc_shifted = pd.DataFrame(shifted_data)
+
+# Plot the first 100 samples of each
+ax = prices_perc_shifted.iloc[:100].plot(cmap=plt.cm.viridis)
+prices_perc.iloc[:100].plot(color='r', lw=2)
+ax.legend(loc='best')
+plt.show()
