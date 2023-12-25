1b189c1ed6b3d4b34edba760bb2b7d5c1ab68887
Sabin <spradh@users.noreply.github.com>
2023-12-25 11:08:16 -0600

    Create 07 Time-based cross-validation.py

---
+++ 4-Validating-and-Inspecting-Time-Series-Models/07 Time-based cross-validation.py
@@ -0,0 +1,14 @@
+# Import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit
+
+# Create time-series cross-validation object
+cv = TimeSeriesSplit(n_splits=10)
+
+# Iterate through CV splits
+fig, ax = plt.subplots()
+for ii, (tr, tt) in enumerate(cv.split(X, y)):
+    # Plot the training data on each iteration, to see the behavior of the CV
+    ax.plot(tr, ii + y[tr])
+
+ax.set(title='Training data on each CV iteration', ylabel='CV iteration')
+plt.show()
