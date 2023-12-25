2865b7ec0ca9ca9d11bbd49fde13a0e2fc9684b9
Sabin <spradh@users.noreply.github.com>
2023-12-24 21:58:22 -0600

    Create 06 Imputing missing values.py

---
+++ 3-Predicting-Time-Series-Data/06 Imputing missing values.py
@@ -0,0 +1,16 @@
+# Create a function we'll use to interpolate and plot
+def interpolate_and_plot(prices, interpolation):
+
+    # Create a boolean mask for missing values
+    missing_values = prices.isnull()
+
+    # Interpolate the missing values
+    prices_interp = prices.interpolate(interpolation)
+
+    # Plot the results, highlighting the interpolated values in black
+    fig, ax = plt.subplots(figsize=(10, 5))
+    prices_interp.plot(color='k', alpha=.6, ax=ax, legend=False)
+    
+    # Now plot the interpolated values on top in red
+    prices_interp[missing_values].plot(ax=ax, color='r', lw=3, legend=False)
+    plt.show()
