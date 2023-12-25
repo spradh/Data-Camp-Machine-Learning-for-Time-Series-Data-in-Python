82d3ac6ccf9fb74e4c7edb1b262c9c2ddc80ccf3
Sabin <spradh@users.noreply.github.com>
2023-12-24 21:20:22 -0600

    Update 01 Introducing the dataset.py

--- 3-Predicting-Time-Series-Data/01 Introducing the dataset.py
+++ 3-Predicting-Time-Series-Data/01 Introducing the dataset.py
@@ -1,1 +1,14 @@
+# Plot the raw values over time
+prices.plot()
+plt.show()
 
+
+# Scatterplot with one company per axis
+print(prices.columns)
+prices.plot.scatter('EBAY', 'YHOO')
+plt.show()
+
+# Scatterplot with color relating to time
+prices.plot.scatter('EBAY', 'YHOO', c=prices.index, 
+                    cmap=plt.cm.viridis, colorbar=False)
+plt.show()
