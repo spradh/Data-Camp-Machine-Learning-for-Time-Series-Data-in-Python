0b035207c23606698cc08b2c0ed88cd88eb025df
Sabin <spradh@users.noreply.github.com>
2023-12-24 22:12:34 -0600

    Create 08 Engineering multiple rolling features at once.py

---
+++ 3-Predicting-Time-Series-Data/08 Engineering multiple rolling features at once.py
@@ -0,0 +1,14 @@
+# Define a rolling window with Pandas, excluding the right-most datapoint of the window
+prices_perc_rolling = prices_perc.rolling(20, min_periods=5, closed='right')
+
+# Define the features you'll calculate for each window
+features_to_calculate = [np.min, np.max, np.mean, np.std]
+
+# Calculate these features for your rolling window object
+features = prices_perc_rolling.aggregate(features_to_calculate)
+
+# Plot the results
+ax = features.loc[:"2011-01"].plot()
+prices_perc.loc[:"2011-01"].plot(ax=ax, color='k', alpha=.2, lw=3)
+ax.legend(loc=(1.01, .6))
+plt.show()
