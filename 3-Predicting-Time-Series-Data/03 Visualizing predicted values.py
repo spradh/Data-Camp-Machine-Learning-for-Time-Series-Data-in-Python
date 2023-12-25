a8754570ba2b0afd26f4acc3876a5b6fe0438520
Sabin <spradh@users.noreply.github.com>
2023-12-24 21:29:57 -0600

    Create 03 Visualizing predicted values.py

---
+++ 3-Predicting-Time-Series-Data/03 Visualizing predicted values.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+# Split our data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    train_size=.8, shuffle=False)
+
+# Fit our model and generate predictions
+model = Ridge()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+score = r2_score(y_test, predictions)
+print(score)
+
+# Visualize our predictions along with the "true" values, and print the score
+fig, ax = plt.subplots(figsize=(15, 5))
+ax.plot(y_test, color='k', lw=3)
+ax.plot(predictions, color='r', lw=2)
+plt.show()
