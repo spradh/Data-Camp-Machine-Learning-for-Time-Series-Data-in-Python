517bd5abe9d2ce301b1c97ba0aa1b9c329a54d92
Sabin <spradh@users.noreply.github.com>
2023-12-24 21:59:49 -0600

    Create 06 Transforming raw data.py

---
+++ 3-Predicting-Time-Series-Data/06 Transforming raw data.py
@@ -0,0 +1,14 @@
+# Your custom function
+def percent_change(series):
+    # Collect all *but* the last value of this window, then the final value
+    previous_values = series[:-1]
+    last_value = series[-1]
+
+    # Calculate the % difference between the last value and the mean of earlier values
+    percent_change = (last_value - np.mean(previous_values)) / np.mean(previous_values)
+    return percent_change
+
+# Apply your custom function and plot
+prices_perc = prices.rolling(20).aggregate(percent_change)
+prices_perc.loc["2014":"2015"].plot()
+plt.show()
