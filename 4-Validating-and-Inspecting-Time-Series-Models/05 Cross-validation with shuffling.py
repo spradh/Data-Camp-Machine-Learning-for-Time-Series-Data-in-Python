211617f4748e60fd765cec09100b336974270d23
Sabin <spradh@users.noreply.github.com>
2023-12-25 10:57:50 -0600

    Create 05 Cross-validation with shuffling.py

---
+++ 4-Validating-and-Inspecting-Time-Series-Models/05 Cross-validation with shuffling.py
@@ -0,0 +1,17 @@
+# Import ShuffleSplit and create the cross-validation object
+from sklearn.model_selection import ShuffleSplit
+cv = ShuffleSplit(n_splits=10, random_state=1)
+
+# Iterate through CV splits
+results = []
+for tr, tt in cv.split(X, y):
+    # Fit the model on training data
+    model.fit(X[tr], y[tr])
+    
+    # Generate predictions on the test data, score the predictions, and collect
+    prediction = model.predict(X[tt])
+    score = r2_score(y[tt], prediction)
+    results.append((prediction, score, tt))
+
+# Custom function to quickly visualize predictions
+visualize_predictions(results)
