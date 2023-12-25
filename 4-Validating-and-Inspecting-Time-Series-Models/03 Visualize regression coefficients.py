97c3d9ec799f9d2c65d1acb6725cf074fbda692d
Sabin <spradh@users.noreply.github.com>
2023-12-25 10:50:53 -0600

    Update 03 Visualize regression coefficients.py

--- 4-Validating-and-Inspecting-Time-Series-Models/03 Visualize regression coefficients.py
+++ 4-Validating-and-Inspecting-Time-Series-Models/03 Visualize regression coefficients.py
@@ -1,8 +1,16 @@
 def visualize_coefficients(coefs, names, ax):
     # Make a bar plot for the coefficients, including their names on the x-axis
-    ax.bar(names, coef)
+    ax.bar(names, coefs)
     ax.set(xlabel='Coefficient name', ylabel='Coefficient value')
     
     # Set formatting so it looks nice
     plt.setp(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
     return ax
+
+# Visualize the output data up to "2011-01"
+fig, axs = plt.subplots(2, 1, figsize=(10, 5))
+y.loc[:'2011-01'].plot(ax=axs[0])
+
+# Run the function to visualize model's coefficients
+visualize_coefficients(model.coef_, prices_perc_shifted.columns, ax=axs[1])
+plt.show()
