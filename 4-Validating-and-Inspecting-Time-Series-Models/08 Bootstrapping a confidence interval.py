7cceefc52ff8e98deb8f102333534b748c946b45
Sabin <spradh@users.noreply.github.com>
2023-12-25 18:12:09 -0600

    Create 08 Bootstrapping a confidence interval.py

---
+++ 4-Validating-and-Inspecting-Time-Series-Models/08 Bootstrapping a confidence interval.py
@@ -0,0 +1,14 @@
+from sklearn.utils import resample
+
+def bootstrap_interval(data, percentiles=(2.5, 97.5), n_boots=100):
+    """Bootstrap a confidence interval for the mean of columns of a 2-D dataset."""
+    # Create our empty array to fill the results
+    bootstrap_means = np.zeros([n_boots, data.shape[-1]])
+    for ii in range(n_boots):
+        # Generate random indices for our data *with* replacement, then take the sample mean
+        random_sample = resample(data)
+        bootstrap_means[ii] = random_sample.mean(axis=0)
+        
+    # Compute the percentiles of choice for the bootstrapped means
+    percentiles = np.percentile(bootstrap_means, percentiles, axis=0)
+    return percentiles
